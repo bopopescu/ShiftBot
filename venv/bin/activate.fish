ea2a5cc27a3ae84b674110ca6402d29d8317fa79
rabhareit <yusuke43z@outlook.com>
2019-12-13 15:56:36 +0900

    venv構築

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/yusuk/PycharmProjects/ShiftChecker/venv"
+set -gx VIRTUAL_ENV "/home/yusuk/app/ShiftBot/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
