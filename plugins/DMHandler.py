e47cc98de13b645f9e7ff4151f0df3cb973d4fd5
rabhareit <yusuke43z@outlook.com>
2019-12-19 14:12:28 +0900

    リマインダー(ダイレクトメッセージ)機能を実装

--- plugins/DMHandler.py
+++ plugins/DMHandler.py
@@ -1,25 +1,28 @@
-from slackbot.slackclient import SlackClient as sc
+from slackbot.slackclient import SlackClient as client
 
 from plugins import Repository as repo
 
 
-def sendReminder4TrashDuty(client):
+def sendReminder4TrashDuty(token):
+    sc = client(token)
     text = '明日のごみ捨て当番です. よろしくお願いします. '
     id2525 = repo.getSlackIDofTrashDuty('2525')
     id2721 = repo.getSlackIDofTrashDuty('2721')
-    sc.send_message(self=client, channel=id2525, message=text)
-    sc.send_message(self=client, channel=id2721, message=text)
+    client.send_message(self=sc, channel=id2525, message=text)
+    client.send_message(self=sc, channel=id2721, message=text)
 
 
-def sendReminder4FinishTrashDuty(client):
-    text = "ごみ捨ては終わりましたか? 完了していれば「部屋番号(半角)+ごみ+終」を含む文を送信してください."
+def sendReminder4TodaysTrashDuty(token):
+    sc = client(token)
+    text = "本日のごみ捨て当番です．よろしくお願いします．"
     id2525 = repo.getSlackIDofTrashDuty('2525')
     id2721 = repo.getSlackIDofTrashDuty('2721')
-    sc.send_message(self=client, channel=id2525, message=text)
-    sc.send_message(self=client, channel=id2721, message=text)
+    client.send_message(self=sc, channel=id2525, message=text)
+    client.send_message(self=sc, channel=id2721, message=text)
 
 
-def sendReminder4MinutesDuty(client, *grade):
+def sendReminder4MinutesDuty(token, *grade):
+    sc = client(token)
     text = '本日の議事録当番です. よろしくお願いします. '
     userid = repo.getSlackIDofMinutesDuty(grade)
-    sc.send_message(self=client, channel=userid, message=text)
+    client.send_message(self=sc, channel=userid, message=text)
--- run.py
+++ run.py
@@ -4,29 +4,32 @@ from slackbot.slackclient import SlackClient as client
 from plugins import DMHandler as dm
 import configparser
 import sys
-import schedule, time
+import schedule
+import time
+import multiprocessing
 
 sys.dont_write_bytecode = True
-
-
 conf = configparser.ConfigParser()
 conf.read('./config.ini')
 API_TOKEN = conf['slack']['BOT_USER_OAUTH_ACCESS_TOKEN']
 
 
 def main():
-    # cl = client(API_TOKEN)
-    # schedule.every().monday.at('10:00').do(dm.sendReminder4MinutesDuty, cl, 'b4')
-    # schedule.every().wednesday.at('18:00').do(dm.sendReminder4TrashDuty, cl)
-    # schedule.every().wednesday.at('10:00').do(dm.sendReminder4MinutesDuty, cl, 'm1')
-
-    # while True:
-    #     schedule.run_pending()
-    #     time.sleep(1)
-
     bot = Bot()
     bot.run()
 
+
+def sendingSchedule():
+    schedule.every().monday.at('10:00').do(dm.sendReminder4MinutesDuty, API_TOKEN, 'b4')
+    schedule.every().thursday.at('11:00').do(dm.sendReminder4TodaysTrashDuty, API_TOKEN)
+    schedule.every().wednesday.at('10:00').do(dm.sendReminder4MinutesDuty, API_TOKEN, 'm1')
+    
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
 if __name__ == '__main__':
     print('Start slackBot')
-    main()
+    multiprocessing.Process(target=main).start()
+    multiprocessing.Process(target=sendingSchedule).start()
